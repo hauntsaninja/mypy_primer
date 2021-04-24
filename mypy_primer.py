6249468ba800a5b7768dda2efdab0494cd2add1f
Akuli <akuviljanen17@gmail.com>
2021-04-25 00:49:17 +0300

    add paroxython (#9)

--- mypy_primer.py
+++ mypy_primer.py
@@ -1225,6 +1225,11 @@ PROJECTS = [
         mypy_cmd="{mypy} --config python/mypy.ini python/pyspark",
         expected_success=True,
     ),
+    Project(
+        location="https://github.com/laowantong/paroxython.git",
+        mypy_cmd="{mypy} paroxython",
+        expected_success=True,
+    ),
     # failures expected...
     Project(
         location="https://github.com/pyppeteer/pyppeteer.git",
