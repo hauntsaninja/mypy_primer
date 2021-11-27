d4adf3ebe71691d653af2d04e9e202bbd7ec4e06
hauntsaninja <>
2021-11-26 20:28:54 -0800

    fix zulip

--- mypy_primer.py
+++ mypy_primer.py
@@ -1047,7 +1047,7 @@ PROJECTS = [
     Project(
         location="https://github.com/zulip/zulip.git",
         mypy_cmd=(
-            "{mypy} zerver zilencer zproject zthumbor tools analytics corporate scripts"
+            "{mypy} zerver zilencer zproject tools analytics corporate scripts"
             " --platform=linux"
         ),
         expected_success=True,
