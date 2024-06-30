5b06f59a25b34c871fad31ec1a715d76f8075a3c
hauntsaninja <hauntsaninja@gmail.com>
2024-06-29 19:08:19 -0700

    fix python version in test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -ex
 isort --diff --check --quiet .
 black --diff --check --quiet .
 flake8 --max-line-length=100 --ignore=E203,W503 $(git ls-files | grep "py$")
-mypy -p mypy_primer --strict --python-version 3.9
+mypy -p mypy_primer --strict --python-version 3.10
 # check we have unique projects
 python -c 'from mypy_primer.projects import get_projects; get_projects()'
 # this check was meant to ensure we could programmatically update the projects
