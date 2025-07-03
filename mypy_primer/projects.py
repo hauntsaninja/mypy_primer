8c03f87aa6352f5710266a95c293149ae7daf61a
Joren Hammudoglu <jhammudoglu@gmail.com>
2025-07-03 12:27:04 +0200

    add missing trailing comma
    (rookie mistake)

--- mypy_primer/projects.py
+++ mypy_primer/projects.py
@@ -781,7 +781,7 @@ def get_projects() -> list[Project]:
                 "packaging",
                 "pandas-stubs",
                 "pydantic",
-                "scipy-stubs"
+                "scipy-stubs",
                 "typeguard",
                 "types-click",
                 "types-PyYAML",
