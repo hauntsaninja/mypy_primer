9784871308ecb3fb4a3619873afb823ea6d4bda3
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2026-09-25 08:02:23 -0700

    treat line-less mypy errors like internal errors

--- mypy_primer/model.py
+++ mypy_primer/model.py
@@ -536,10 +536,13 @@ class PrimerResult:
         # mypy's output appears to be nondeterministic for some same line errors, e.g. on pypa/pip
         # Work around that by ignoring identical removal and addition pairs, e.g.
         # "- a.py:1: error xyz" and "+ a.py:1: error xyz"
+        # Keep internal errors and startup errors without a source location even if unchanged.
         diff_lines = [
             line
             for line in d.compare(old_lines, new_lines)
-            if line[0] in ("+", "-") or "INTERNAL ERROR" in line
+            if line[0] in ("+", "-")
+            or "INTERNAL ERROR" in line
+            or line.startswith(("  error:", "  mypy: error:"))
         ]
         net_change: dict[str, int] = defaultdict(int)
         for line in diff_lines:
